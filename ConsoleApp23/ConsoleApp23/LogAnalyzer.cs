53c3d4a0ac8e7e5fb26582345fc3abbc8089fec1
howard <sdr40725@gmail.com>
2020-03-25 10:34:13 +0800

    115

---
+++ ConsoleApp23/ConsoleApp23/LogAnalyzer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LogAnalyzer
+{
+    public class LogAnalyzer
+    {
+        public bool IsValidLogFileName(string fileName)
+        {
+            if (!fileName.EndsWith(".log", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ConsoleApp23/LogAnalyzer.Tests/LogAnalyzer.Tests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using ConsoleApp23;
+
+
+
+
+namespace LogAnalyzer.Tests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidLogFilename_BadExtension_ReturnsFalse()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName("test.l2og");
+            Assert.False(result);
+        }
+
+        [Test]
+        public void IsValidLogFileName_googExtensionUppercase_ReturnsTrue()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName("test.log");
+            Assert.True(result);
+        }
+
+    }
+}
